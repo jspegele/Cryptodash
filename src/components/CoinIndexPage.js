36afd7d4d2a9661ae60388995750237e0273699f
Justin Spegele <justin.spegele@gmail.com>
2020-05-03 16:04:03 -0400

    Added infinite scroll to list

--- src/components/CoinIndexPage.js
+++ src/components/CoinIndexPage.js
@@ -1,12 +1,44 @@
 import React, { useContext } from 'react'
+import { FaCircleNotch } from 'react-icons/fa'
 import CoinsContext from '../context/coins-context'
 import CoinTile from './CoinTile'
 
 class DashboardPage extends React.Component {
+  state ={
+    sliceSize: 50,
+    nextCoinIndex: 0,
+    loading: false
+  }
+  componentDidMount = () => {
+    document.addEventListener('scroll', this.trackScrolling);
+  }
+  componentDidUpdate = () => {
+    document.addEventListener('scroll', this.trackScrolling);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.trackScrolling);
+  }
+  isBottom(el) {
+    return el.getBoundingClientRect().bottom <= window.innerHeight;
+  }
+  trackScrolling = () => {
+    const wrappedElement = document.getElementById('coin-list');
+    if (this.isBottom(wrappedElement)) {
+      this.setState(() => ({ loading: true }))
+      document.removeEventListener('scroll', this.trackScrolling);
+      const self = this;
+      setTimeout(function() {
+        self.setState(() => ({
+          nextCoinIndex: self.state.nextCoinIndex + self.state.sliceSize,
+          loading: false
+        }))
+      }, 2000);
+    }
+  }
   render() {
     return (
       <div id="coin-list" className="index">
-        <div className="index-header">
+        <div className="index__header">
           <div className="text-center">#</div>
           <div>Coin</div>
           <div className="text-right">Price</div>
@@ -17,14 +49,15 @@ class DashboardPage extends React.Component {
         <CoinsContext.Consumer>
           {({ coins }) => (
             coins.length > 0 ? (
-              coins.slice(0, 50).map((coin, index) => (
+              coins.slice(0, (this.state.nextCoinIndex + this.state.sliceSize - 1)).map((coin, index) => (
                 <CoinTile key={coin.symbol} index={index} coin={coin} />
               ))
             ) : (
-              <div>Loading coin data...</div>
+              <div className="index__notification">Loading coin data<FaCircleNotch size="2.4rem" className="fa-spin" /></div>
             )
           )}
         </CoinsContext.Consumer>
+        {this.state.loading && <div className="index__notification">Fetching more coins<FaCircleNotch size="2.4rem" className="fa-spin" /></div>}
       </div>
     )
   }
