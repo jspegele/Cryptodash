01e955292b5020b50fd592abff36e7a811ed9861
Justin Spegele <justin.spegele@gmail.com>
2020-05-08 14:46:52 -0400

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => (
+  <div className="content-container">
+    <div className="footer">
+      <div>Created by <a href="http://justinspegele.com">Justin Spegele</a> | <a href="https://github.com/jspegele">GitHub</a></div>
+    </div>
+  </div>
+)
+
+export default Footer
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 import DashboardPage from '../components/DashboardPage'
 import CoinIndexPage from '../components/CoinIndexPage'
 import SettingsPage from '../components/SettingsPage'
@@ -18,6 +19,7 @@ const AppRouter = () => (
       <Route path="/coin/:symbol" component={CoinPage} />
       <Route path="/settings" component={SettingsPage} />
     </Switch>
+    <Footer />
   </Router>
 );
 
